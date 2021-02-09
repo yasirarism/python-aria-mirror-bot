e367789313aabb07fd26273fc087b5f234d6e370
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-02-09 08:38:35 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -118,17 +118,17 @@ def get_readable_time(seconds: int) -> str:
     (days, remainder) = divmod(seconds, 86400)
     days = int(days)
     if days != 0:
-        result += f'{days}d'
+        result += f'{hari}d'
     (hours, remainder) = divmod(remainder, 3600)
     hours = int(hours)
     if hours != 0:
-        result += f'{hours}h'
+        result += f'{jam}h'
     (minutes, seconds) = divmod(remainder, 60)
     minutes = int(minutes)
     if minutes != 0:
-        result += f'{minutes}m'
+        result += f'{menit}m'
     seconds = int(seconds)
-    result += f'{seconds}s'
+    result += f'{detik}s'
     return result
 
 
