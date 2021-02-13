8f8869915f1764388b071af11651a786ebf97fe7
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-02-13 18:50:42 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -129,7 +129,7 @@ class MirrorListener(listeners.MirrorListeners):
             uname = f"@{self.message.from_user.username}"
         else:
             uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
-        msg = f"Hai {uname}, kayaknya ini bukan direct link deh 🤔, coba cari link yang aku bisa download"
+        msg = f"Hai {uname}, kayaknya ada error nih karena : {error}"
         sendMessage(msg, self.bot, self.update)
         if count == 0:
             self.clean()
